4caa4f05069d458c7cf865224085f4bc73fdf232
sonalib7722 <sonalib7722@gmail.com>
2023-02-28 13:32:03 +0530

    chnages for additional methods

--- productservice/src/main/java/com/sb/product/controller/ProductController.java
+++ productservice/src/main/java/com/sb/product/controller/ProductController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +25,17 @@ public class ProductController {
 	}
 	
 	@RequestMapping(value="/getproducts",method=RequestMethod.GET)
-	public List<Products> getCertificationsList() {
+	public List<Products> getproductList() {
 		return repository.findAll();
+	}
+	
+	@RequestMapping(value="/addproduct",method=RequestMethod.POST)
+	public Products saveProduct(@RequestBody Products product) {
+		return repository.save(product);
+	}
+	
+	@RequestMapping(value="/getproductdetails/{productid}",method=RequestMethod.GET)
+	public Products getProductDetails(@PathVariable String productid) {
+		return repository.findById(productid).get();
 	}
 }
