438c3475f40aa536538a36d1d3ca181a2c22115a
sonalib7722 <sonalib7722@gmail.com>
2023-02-28 12:19:35 +0530

    tests

--- productservice/src/main/java/com/sb/controller/ProductController.java
+++ productservice/src/main/java/com/sb/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.sb.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,11 @@ public class ProductController {
 	
 	@Autowired
 	ProductRepository repository;
+	
+	@GetMapping("/get")
+	public String getmessage() {
+		return "yayayy";
+	}
 	
 	@RequestMapping(value="/getproducts",method=RequestMethod.GET)
 	public List<Products> getCertificationsList() {
