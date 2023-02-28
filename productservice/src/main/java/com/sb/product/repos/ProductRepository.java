af616cf84af9884c3451aa170b18cde3274fea0b
sonalib7722 <sonalib7722@gmail.com>
2023-02-28 12:04:46 +0530

    remove

--- productservice/src/main/java/com/sb/product/repos/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sb.product.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sb.product.model.Products;
-
-public interface ProductRepository extends JpaRepository<Products, String> {
-
-}
